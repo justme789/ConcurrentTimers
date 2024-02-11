ff84433edb89351af16628234faf2cbaf8f9c352
justme789 <nobodyxoxo123@gmail.com>
2024-02-11 16:02:23 +0000

    Add project files.

---
+++ Timers/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Timers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<TimersContainer> timers = new ObservableCollection<TimersContainer>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            TimersList.ItemsSource = timers;
+        }
+
+        private void Border_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.A)
+            {
+                Random rnd = new Random();
+                timers[rnd.Next(timers.Count)].Timers.Add(new TimerSituation(rnd.Next(100)));
+            }
+            if (e.Key == Key.C)
+            {
+                Random rnd = new Random();
+                timers.Add(new TimersContainer());
+            }
+        }
+
+        private void Border_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ((TimersContainer)((Border)sender).DataContext).Timers.Add(new TimerSituation(100));
+        }
+
+        private void Border_MouseUp_1(object sender, MouseButtonEventArgs e)
+        {
+            timers.Add(new TimersContainer());
+        }
+
+        private void Grid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((TimerSituation)((Grid)sender).DataContext).Hovered = true;
+        }
+
+        private void Grid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((TimerSituation)((Grid)sender).DataContext).Hovered = false;
+        }
+    }
+}
---
+++ Timers/TimersContainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Timers
+{
+    public class TimersContainer
+    {
+        DispatcherTimer timer;
+        public ObservableCollection<TimerSituation> Timers { get; set; } = new ObservableCollection<TimerSituation>();
+        public TimersContainer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (Timers.Count > 0)
+            {
+                TimerSituation currentTimer = Timers[0];
+                if (currentTimer.CurrVal <= 0)
+                {
+                    Timers.RemoveAt(0);
+                    return;
+                }
+                currentTimer.CurrVal--;
+                currentTimer.DisplayVal = currentTimer.CurrVal + "";
+            }
+        }
+    }
+
+}
